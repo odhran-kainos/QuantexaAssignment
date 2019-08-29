03e7c2e2268d14280bae754e99289decb07fceac
Odhran Hardy <o.hardy@kainos.com>
2019-08-29 07:00:36 +0100

    Adding answers to assignment 3

--- src/main/scala/com/quantexa/assignments/transactions/TransactionsAssignment.scala
+++ src/main/scala/com/quantexa/assignments/transactions/TransactionsAssignment.scala
@@ -4,6 +4,8 @@
 
 package com.quantexa.assignments.transactions
 
+import com.quantexa.assignments.transactions.TransactionAssignment.transactions
+
 import scala.annotation.tailrec
 import scala.io.Source
 import scala.util.Try
@@ -13,7 +15,7 @@ import scala.util.Try
   * Once executed each question is executed in turn printing results to console
   */
   
-object TransactionAssignment {
+object TransactionAssignment extends App{
 
   /***
     * A case class to represent a transaction
@@ -28,7 +30,28 @@ object TransactionAssignment {
                           accountId: String,
                           transactionDay: Int,
                           category: String,
-                          transactionAmount: Double)
+                          transactionAmount: Double
+                        )
+
+  case class Question1Result(
+                             transactionDay: Int,
+                             transactionTotal: Double
+                            )
+
+  case class Question2Result(
+                              accountId: String,
+                              categoryAvgValueMap: Map[String, Double]
+                            )
+
+  case class Question3Result(
+                              transactionDay: Int,
+                              accountId: String,
+                              max: Double,
+                              avg: Double,
+                              aaTotal: Double,
+                              ccTotal: Double,
+                              ffTotal: Double
+                            )
 
   //The full path to the file to import
   val fileName = getClass.getResource("/transactions.csv").getPath
@@ -49,6 +72,65 @@ object TransactionAssignment {
    * 
    */
 
- 
+  /*
+  Question 1 - Total transaction value for all transactions for each day
+   */
+  def getTotalTransactionsPerDay(transactions: List[Transaction]): Seq[Question1Result] = {
+    transactions.groupBy(_.transactionDay)
+      .map(x => Question1Result(x._1, x._2.map(_.transactionAmount).sum))
+      .toSeq
+      .sortBy(_.transactionDay)
+  }
 
+  val question1ResultValue = getTotalTransactionsPerDay(transactions)
+  question1ResultValue.foreach(println)
+
+  /*
+  Question 2 - Average value of transactions per account for each type of transaction
+   */
+  def getAverageTransactionsPerAccountByType(transactions: List[Transaction]): Seq[Question2Result] = {
+    transactions.groupBy(trans => (trans.accountId, trans.category))
+      .mapValues(_.map(_.transactionAmount))
+      .mapValues(amounts => amounts.sum/amounts.size)
+      .map(x => Question2Result(x._1._1, Map(x._1._2 -> x._2)))
+      .toSeq
+      .sortBy(_.accountId)
+  }
+
+  val question2ResultValue = getAverageTransactionsPerAccountByType(transactions)
+  question2ResultValue.foreach(println)
+  /*
+  Question 3 - Statistics for each account number for the previous five days
+   */
+  def getStatsPerAccountForLastFiveDays(transactions: List[Transaction]) = {
+    val windowSize = 5
+
+    val transWindow = transactions.groupBy(trans =>(trans.transactionDay, trans.accountId))
+      .toList.sortBy(x => (x._1._1, x._1._2))
+      .sliding(windowSize)
+
+    val statistics: Iterator[Iterable[Question3Result]] =
+      for ((transactionsPeriod, index) <- transWindow.zipWithIndex) yield {
+        val day = index + windowSize + 1
+        val transactionsFromPeriod = transactionsPeriod.flatMap(_._2)
+        val transactionsFromPeriodByAccount = transactionsFromPeriod.groupBy(_.accountId)
+
+        val stats: Iterable[Question3Result] = transactionsFromPeriodByAccount.map {
+          case (accountId, t) =>
+            Question3Result(
+              day,
+              accountId,
+              t.map(_.transactionAmount).max,
+              t.map(_.transactionAmount).sum / t.size,
+              t.filter(_.category == "AA").map(_.transactionAmount).sum,
+              t.filter(_.category == "CC").map(_.transactionAmount).sum,
+              t.filter(_.category == "FF").map(_.transactionAmount).sum)
+        }
+        stats
+      }
+    statistics.flatten.toSeq
+  }
+
+  val question3ResultValue = getStatsPerAccountForLastFiveDays(transactions)
+  question3ResultValue.foreach(println)
 }
